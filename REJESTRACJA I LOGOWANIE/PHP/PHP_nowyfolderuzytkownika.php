9ecb81621f799bbe85d648436730189b2aba95eb
MagiczneMaselko <arturzakrzewski96@gmail.com>
2023-05-20 20:13:16 +0200

    modified:   REJESTRACJA I LOGOWANIE/PHP/PHP_nowyfolderuzytkownika.php 	new file:   ZALOGOWANY/.TEMPLATE_KANALU/dodawanie_do_tabeli.php 	new file:   ZALOGOWANY/.TEMPLATE_KANALU/golomp.jpg 	modified:   ZALOGOWANY/.TEMPLATE_KANALU/index.php 	new file:   ZALOGOWANY/.TEMPLATE_KANALU/pobierz.php 	modified:   ZALOGOWANY/.TEMPLATE_KANALU/style.css 	new file:   ZALOGOWANY/.TEMPLATE_KANALU/tf.jpg 	new file:   ZALOGOWANY/.TEMPLATE_KANALU/usun.php 	new file:   ZALOGOWANY/.TEMPLATE_KANALU/wyslij.php 	new file:   ZALOGOWANY/.TEMPLATE_KANALU/wyswietlanie_tabeli.php 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/dodaj_kanal.php 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/edycja_kanalu.php 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/golomp.jpg 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/kopiowanie_szablony_foldera.php 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/style.css 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/tabela.php 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/index.php 	renamed:    ZALOGOWANY/.TEMPLATE_KANALU/patchnotes.html -> ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/patchnotes.html 	renamed:    ZALOGOWANY/.TEMPLATE_KANALU/strona_w_budowie.html -> ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/strona_w_budowie.html 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/style.css 	new file:   ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/wyswietlanie_kanalow.php

--- REJESTRACJA I LOGOWANIE/PHP/PHP_nowyfolderuzytkownika.php
+++ REJESTRACJA I LOGOWANIE/PHP/PHP_nowyfolderuzytkownika.php
@@ -1,7 +1,17 @@
 <?php
     $nazwa_folderu = $_POST['login'];
     mkdir("./../ZALOGOWANY/$nazwa_folderu");
-    copy("./../ZALOGOWANY/.TEMPLATE_KANALU/index.php", "./../ZALOGOWANY/$nazwa_folderu/index.php");
-    copy("./../ZALOGOWANY/.TEMPLATE_KANALU/patchnotes.html", "./../ZALOGOWANY/$nazwa_folderu/patchnotes.html");
-    copy("./../ZALOGOWANY/.TEMPLATE_KANALU/strona_w_budowie.html", "./../ZALOGOWANY/$nazwa_folderu/strona_w_budowie.html");
-    copy("./../ZALOGOWANY/.TEMPLATE_KANALU/style.css", "./../ZALOGOWANY/$nazwa_folderu/style.css");
+    mkdir("./../ZALOGOWANY/$nazwa_folderu/KANALY");
+    
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/index.php", "./../ZALOGOWANY/$nazwa_folderu/index.php");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/patchnotes.html", "./../ZALOGOWANY/$nazwa_folderu/patchnotes.html");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/strona_w_budowie.html", "./../ZALOGOWANY/$nazwa_folderu/strona_w_budowie.html");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/style.css", "./../ZALOGOWANY/$nazwa_folderu/style.css");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/wyswietlanie_kanalow.php", "./../ZALOGOWANY/$nazwa_folderu/wyswietlanie_kanalow.php");
+    /////////////////////////////////////////////////////////
+    mkdir("./../ZALOGOWANY/$nazwa_folderu/EDYCJA");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/dodaj_kanal.php", "./../ZALOGOWANY/$nazwa_folderu/EDYCJA/dodaj_kanal.php");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/edycja_kanalu.php", "./../ZALOGOWANY/$nazwa_folderu/EDYCJA/edycja_kanalu.php");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/kopiowanie_szablony_foldera.php", "./../ZALOGOWANY/$nazwa_folderu/EDYCJA/kopiowanie_szablony_foldera.php");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/style.css", "./../ZALOGOWANY/$nazwa_folderu/EDYCJA/style.css");
+    copy("./../ZALOGOWANY/.TEMPLATE_UZYTKOWNIKA/EDYCJA/tabela.php", "./../ZALOGOWANY/$nazwa_folderu/EDYCJA/tabela.php");
---
+++ ZALOGOWANY/.TEMPLATE_KANALU/usun.php
@@ -0,0 +1,6 @@
+<?php
+    if ($_GET["plik"] != "") {
+        $file_url = "./PLIKI/".$_GET["plik"];
+        unlink($file_url); 
+        header("Location: ./index.php");
+    }
